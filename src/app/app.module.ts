76a733ebe27699700d01ed6461c31fb5c4248e57
Umang <umang.srivastava.ckc@gmail.com>
2019-03-03 19:29:35 +0530

    changed app module for better readability.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { NgModule } from '@angular/core';
 // import { HttpClient } from "@angular/common/http";
 import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 //importing MaterialComponentsModule
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+//importing project components & services
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import {MaterialComponentsModule} from './material-components/material-components.module';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { AddWeatherCardComponent } from './add-weather-card/add-weather-card.component';
-
 import { WeatherService } from "./Services/weather/weather.service";
 
 
